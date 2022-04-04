f6854745984d925d9d1635d97ab96b6d80e687c1
skydev <kgc0108@hotmail.com>
2022-04-04 03:21:00 -0400

    remove needless dependencies

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,91 +1,24 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import MiniCSSExtractPlugin from 'mini-css-extract-plugin'
-import path from 'path'
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const webpackCommon = {
-    entry: path.resolve(__dirname, '../src/index.js'),
-    output:
-    {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
-    },
-    devtool: 'source-map',
     plugins:
     [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, '../static') }
-            ]
-        }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
+            favicon: 'src/favicon.png',
+            meta: {
+                'viewport': 'width=device-width, user-scalable=no, minimum-scale=1, maximum-scale=1'
+            },
+            title: 'PlayCanvas Webpack Example',
             minify: true
-        }),
-        new MiniCSSExtractPlugin()
+        })
     ],
     module:
     {
         rules:
         [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
-
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
-            },
-
-            // CSS
             {
                 test: /\.css$/,
-                use:
-                [
-                    MiniCSSExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-
-            // Images
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/images/'
-                        }
-                    }
-                ]
-            },
-
-            // Fonts
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/fonts/'
-                        }
-                    }
-                ]
+                use: ['style-loader', 'css-loader']
             }
         ]
     }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,15 +1,10 @@
 import { merge } from 'webpack-merge'
 import commonConfiguration from './webpack.common.js'
-import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 
 const configuration = merge(
     commonConfiguration,
     {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
+        mode: 'production'
     }
 )
 
